3ccd040879b02bbb06e87c04ee104a6deb0c1212
Yulia <ukalinicenko15@gmail.com>
2021-04-15 20:02:44 +0300

    Fixed index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
-// Проверка того, что наш браузер поддерживает Service Worker API.
 if ('serviceWorker' in navigator) {
-    // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('sw.js', {scope: '/'})
-        .then(() => navigator.serviceWorker.ready.then((worker) => {
-            worker.sync.register('syncdata');
-        }))
-        .catch((err) => console.log(err));
+    // declaring scope manually
+    navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
+        console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function(error) {
+        console.log('Service worker registration failed:', error);
+    });
+} else {
+    console.log('Service workers are not supported.');
 }
